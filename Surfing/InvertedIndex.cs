ee9bbf26cef4287966eed7d03e45c6a367d338fa
olesenmail@gmail.com <bolsson>
2015-11-04 20:03:31 +0100

    added InvertedIndex.cs

---
+++ Surfing/InvertedIndex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InvertedIndex
+{
+    class InvertedIndex
+
+    {
+        Dictionary<string, List<string>> invertedIndex = new Dictionary<string, List<string>>();
+
+        public void Add(string url, List<string> wordsOnPage)
+        {
+            List<string> urlList;
+
+            foreach (var word in wordsOnPage)
+            {
+                if (invertedIndex.ContainsKey(word))
+                {
+                    urlList = invertedIndex[word];
+                    urlList.Add(url);
+                }
+                else
+                {
+                    urlList = new List<string>();
+                    urlList.Add(url);
+                }
+                invertedIndex.Add(word, urlList);
+            }
+        }
+    }
+}
