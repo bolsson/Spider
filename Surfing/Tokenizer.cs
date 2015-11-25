352554988132631b4447396727b352daa9f88555
olesenmail@gmail.com <bolsson>
2015-11-25 21:45:09 +0100

    refactoring, before changing from list to stack

--- Surfing/Tokenizer.cs
+++ Surfing/Tokenizer.cs
@@ -8,5 +8,29 @@ namespace Spider
 {
     class Tokenizer
     {
+
+        private List<string> _tokenList;
+
+        public Tokenizer()
+        {
+            _tokenList = new List<string>();
+        }
+
+        public void AddToken(string token)
+        {
+            _tokenList.Add(token);
+        }
+
+        public string getNextToken()
+        {
+            var firstToken = _tokenList[0];
+            _tokenList.RemoveAt(0);
+            return firstToken;
+        }
+
+        public Boolean isEmpty()
+        {
+            return _tokenList.Count() == 0;
+        }
     }
 }
