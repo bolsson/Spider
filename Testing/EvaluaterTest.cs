b54395d6e0dd74cd57f6abfba86d524b5c2177be
Bolsson <olesenmail@gmail.com>
2016-07-07 18:42:18 +0200

    EvaluaterTest.cs adding setup method

--- Testing/EvaluaterTest.cs
+++ Testing/EvaluaterTest.cs
@@ -13,12 +13,25 @@ namespace Testing
         [SetUp]
         public void Init()
         {
+            string word1 = "word1";
+            string word2 = "word2";
+            string word3 = "word3";
+            string word4 = "word4";
+            string word5 = "word5";
+            string word6 = "word6";
             //Note fill testdata  into the invertedindex -- in progress
             InvertedIndex index = new InvertedIndex();
-            index.Add("", new List<string>() { "", "" });
+            index.Add(word1, new List<string>() { "doc1", "doc2", "doc3", "doc4" });
+            index.Add(word2, new List<string>() { "doc2", "doc4", "doc6", "doc8" });
+            index.Add(word3, new List<string>() { "doc3", "doc6", "doc9", "doc12" });
+            index.Add(word4, new List<string>() { "doc4", "doc8", "doc12", "doc16" });
+            index.Add(word5, new List<string>() { "doc5", "doc10", "doc15", "doc20" });
+            index.Add(word6, new List<string>() { "doc6", "doc12", "doc18", "doc24" });
 
             Tokens tokens = new Tokens();
-            tokens.AddToken(new WordToken("hello world"));
+            tokens.AddToken(new WordToken(word1));
+            tokens.AddToken(new AndToken());
+            tokens.AddToken(new WordToken(word2));
 
             Parser parse = new Parser(tokens);
             Node root = parse.BinaryTree.root;
