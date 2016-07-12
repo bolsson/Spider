83ec7efdf2c3dd91646a6cc1c47f2ce119c4087c
Bolsson <olesenmail@gmail.com>
2016-07-12 09:21:49 +0200

    update files to include at github

---
+++ Search/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Spider;
+
+namespace Search.Controllers
+{
+    public class HomeController : Controller
+    {
+        Tokens tokens = new Tokens();
+        InvertedIndex index;
+
+        public HomeController()
+        {
+            Parser parse = new Parser(tokens);
+            Node root = parse.BinaryTree.root;
+
+            Evaluater eval = new Evaluater(root, index);
+            List<string> res = eval.result.ToList<string>();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
